14acd9dfd453350f84e5603889676057f6604f28
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-08-05 20:24:59 -0400

    Add Track component and CSS

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Track.css';
+
+class Track extends React.Component {
+    render() {
+        return (
+            <div className="TrackList">
+                <!-- You will add a map method that renders a set of Track components  -->
+            </div>
+        );
+    }
+}
+
+export default Track;
