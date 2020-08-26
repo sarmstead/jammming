1b439cfb20aafb8ce189382651e45bfc7a98a0d2
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-08-26 19:58:41 -0400

    Add styling to Jammming logo component.

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -3,6 +3,11 @@ import './Modal.css';
 import Spotify from '../../.util/Spotify';
 import {ReactComponent as Logo} from './jammming_purple.svg';
 
+const logoStyle = {
+    width: '20rem',
+    margin: '0 auto 1.7rem auto'
+};
+
 class Modal extends React.Component {
     getAccessToken() {
         Spotify.getAccessToken();
@@ -11,7 +16,7 @@ class Modal extends React.Component {
     render() {
         return (
             <main>
-                <Logo class="logo" alt="Jamming purple logo" />
+                <Logo class="logo" alt="Jamming purple logo" style={logoStyle} />
                 <p>Welcome to your interactive Spotify companion.</p> 
                 <p class="emphasis">Connect Jammming to your Spotify account to get started.</p>
                 <button onClick={this.getAccessToken}>Connect to Spotify</button>
