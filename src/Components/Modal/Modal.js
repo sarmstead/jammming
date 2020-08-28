9d6b7458c1a649141b95e6e47db982c62e230818
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-08-28 12:56:50 -0400

    Fix Modal className syntax

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -3,11 +3,6 @@ import './Modal.css';
 import Spotify from '../../.util/Spotify';
 import {ReactComponent as Logo} from './jammming_purple.svg';
 
-const logoStyle = {
-    width: '20rem',
-    margin: '0 auto 1.7rem auto'
-};
-
 class Modal extends React.Component {
     getAccessToken() {
         Spotify.getAccessToken();
@@ -19,9 +14,9 @@ class Modal extends React.Component {
         }
         return (
             <main>
-                <Logo class="logo" alt="Jamming purple logo" style={logoStyle} />
+                <Logo className="logo" alt="Jamming purple logo" />
                 <p>Welcome to your interactive Spotify companion.</p> 
-                <p class="emphasis">Connect Jammming to your Spotify account to get started.</p>
+                <p className="emphasis">Connect Jammming to your Spotify account to get started.</p>
                 <button onClick={this.getAccessToken}>Connect to Spotify</button>
             </main>
         );
