d7eeee8ec54b49fcd036b7332b2a62b057302ea1
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-08-29 19:11:42 -0400

    Add event handling for onKeyDown using Enter key

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,13 +5,18 @@ class SearchBar extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            term: 'term'
+            term: ''
         }
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     search() {
+        if (this.state.term === '') {
+            alert('Please enter a song, album, or artist!');
+        }
+
         this.props.onSearch(this.state.term);
     }
 
@@ -19,10 +24,21 @@ class SearchBar extends React.Component {
         this.setState({ term: event.target.value });
     }
 
+    handleKeyPress(event) {
+        if (event.nativeEvent.keyCode === 13 && this.state.term === '') {
+            alert('Please enter a song, album, or artist!');
+        }
+
+        if (event.nativeEvent.keyCode === 13) {
+            this.props.onSearch(this.state.term);
+
+        }
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onKeyDown={this.handleKeyPress} />
                 <button className="SearchButton" onClick={this.search}>SEARCH</button>
             </div>
         );
